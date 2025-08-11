cdeb1b33c549b47a1493c1932de8c6d288cfbf3e
nuoyanLyu <nuoyanlyu@gmail.com>
2025-08-11 20:33:22 +0800

    update config files

--- test_dl.sh
+++ test_dl.sh
@@ -8,5 +8,5 @@ USE_BASE="algorithm.kl_ctrl.kl_coef=0.001 actor_rollout_ref.actor.kl_loss_coef=0
 LOCAL_PATH="/root/autodl-tmp/"
 
 # WANDB_MODE=offline python train.py --config-name _1_bandit system.CUDA_VISIBLE_DEVICES=\"0,4\" trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=bandit-ppo $USE_PPO &
-WANDB_MODE=offline python train.py --config-name _7_connect4 system.CUDA_VISIBLE_DEVICES=\"0,1\" model_path=/root/autodl-tmp/Qwen3-1.7B trainer.default_local_dir=$LOCAL_PATH trainer.total_training_steps=100 trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=connect4-grpo $USE_GRPO &
+WANDB_MODE=offline python train.py --config-name _7_connect4 system.CUDA_VISIBLE_DEVICES=\"0,1\" model_path=/root/autodl-tmp/Qwen3-1.7B trainer.default_local_dir=$LOCAL_PATH trainer.total_training_steps=200 trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=connect4-grpo $USE_GRPO &
 # WANDB_MODE=offline python train.py --config-name _2_sokoban system.CUDA_VISIBLE_DEVICES=\"3,4\" trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=sokoban-grpo actor_rollout_ref.rollout.rollout_filter_ratio=0.25 $USE_GRPO &
