34ff34a59ccc5b3fd59f6011d3338bd4bbe93fb3
nuoyanLyu <nuoyanlyu@gmail.com>
2025-08-13 21:45:41 +0800

    Update scripts for running connect4 env.

--- test_dl.sh
+++ test_dl.sh
@@ -13,5 +13,13 @@ mkdir -p "$LOG_DIR" # 如果目录不存在，则创建它
 TIMESTAMP=$(date +"%m-%d-%H-%M")
 
 # WANDB_MODE=offline python train.py --config-name _1_bandit system.CUDA_VISIBLE_DEVICES=\"0,4\" trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=bandit-ppo $USE_PPO &
-WANDB_MODE=offline python train.py --config-name _7_connect4 system.CUDA_VISIBLE_DEVICES=\"0,1\" model_path=/root/autodl-tmp/Qwen3-1.7B trainer.default_local_dir=$LOCAL_PATH trainer.total_training_steps=200 trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=connect4-grpo $USE_GRPO > "$LOG_DIR/grpo_${TIMESTAMP}.log" 2>&1 &
+WANDB_MODE=offline python train.py --config-name _7_connect4 \
+ system.CUDA_VISIBLE_DEVICES=\"0,1\" \
+ model_path=/root/autodl-tmp/Qwen3-1.7B \
+ trainer.default_local_dir=$LOCAL_PATH \
+ trainer.total_training_steps=200 \
+ trainer.n_gpus_per_node=2 \
+ actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+ trainer.experiment_name=connect4-grpo \
+ $USE_GRPO 2>&1 | tee "$LOG_DIR/grpo_${TIMESTAMP}.log" &
 # WANDB_MODE=offline python train.py --config-name _2_sokoban system.CUDA_VISIBLE_DEVICES=\"3,4\" trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=sokoban-grpo actor_rollout_ref.rollout.rollout_filter_ratio=0.25 $USE_GRPO &
