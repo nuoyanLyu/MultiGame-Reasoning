ee88041ea97753812549fc2b2ec29c3f6bbc5bc7
nuoyanLyu <nuoyanlyu@gmail.com>
2025-09-23 10:01:44 +0800

    Add let's think step by step prompt in training prompt.

--- tictactoe_gemini.sh
+++ tictactoe_gemini.sh
@@ -5,21 +5,20 @@ USE_GRPO="algorithm.adv_estimator=grpo agent_proxy.reward_normalization.method=m
 USE_PPO="algorithm.adv_estimator=gae" # by default.
 USE_BASE="algorithm.kl_ctrl.kl_coef=0.001 actor_rollout_ref.actor.kl_loss_coef=0.001 actor_rollout_ref.actor.clip_ratio_high=0.2 actor_rollout_ref.rollout.rollout_filter_ratio=1"
 
-LOCAL_PATH="/root/autodl-tmp/tictactoe-gemini"
-LOG_DIR="/root/RAGEN/logs/tictactoe-gemini"
+LOCAL_PATH="/root/autodl-tmp/tictactoe-gemini-think"
+LOG_DIR="/root/RAGEN/logs/tictactoe-gemini-think"
 mkdir -p "$LOG_DIR" # 如果目录不存在，则创建它
 
 # 获取当前时间，格式为 YYYY-MM-DD-HHMMSS
 TIMESTAMP=$(date +"%m-%d-%H-%M")
 
-# WANDB_MODE=offline python train.py --config-name _1_bandit system.CUDA_VISIBLE_DEVICES=\"0,4\" trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=bandit-ppo $USE_PPO &
 WANDB_MODE=offline RAY_DEDUP_LOGS=0 python train.py --config-name _8_tictactoe \
  system.CUDA_VISIBLE_DEVICES=\"0,1\" \
  model_path=/root/autodl-tmp/Qwen2.5-1.5B-Instruct \
  trainer.default_local_dir=$LOCAL_PATH \
- trainer.total_training_steps=100 \
+ trainer.total_training_steps=400 \
  trainer.n_gpus_per_node=2 \
  actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
- trainer.experiment_name=tictactoe-gemini-grpo \
+ trainer.experiment_name=tictactoe-gemini-think \
  $USE_GRPO 2>&1 | tee "$LOG_DIR/grpo_${TIMESTAMP}.log" &
 # WANDB_MODE=offline python train.py --config-name _2_sokoban system.CUDA_VISIBLE_DEVICES=\"3,4\" trainer.n_gpus_per_node=2 actor_rollout_ref.rollout.tensor_model_parallel_size=2 trainer.experiment_name=sokoban-grpo actor_rollout_ref.rollout.rollout_filter_ratio=0.25 $USE_GRPO &
