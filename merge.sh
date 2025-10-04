3ab467852aee31f4c30c4a625a0a727b5a4e0849
nuoyanLyu <nuoyanlyu@gmail.com>
2025-10-04 11:24:22 +0800

    Update test files and math training settings.

--- merge.sh
+++ merge.sh
@@ -1,5 +1,5 @@
-ORI_PATH="/root/autodl-tmp/tictactoe-gemini-think/global_step_300/actor"
-TAR_PATH="/root/autodl-tmp/tictactoe-gemini-think/game300"
+ORI_PATH="/root/autodl-tmp/math/global_step_200/actor"
+TAR_PATH="/root/autodl-tmp/math/math200"
 HF_PATH="/root/autodl-tmp/Qwen2.5-1.5B-Instruct"
 
 python verl/scripts/model_merger.py --backend fsdp --local_dir $ORI_PATH --target_dir $TAR_PATH --hf_model_path $HF_PATH
--- reason_test/lv3to5_dl.py
+++ reason_test/lv3to5_dl.py
@@ -1,4 +1,3 @@
-# 根据infer得到的结果，运行函数提取模型生成的信息
 import json
 import re
 import time
@@ -11,9 +10,10 @@ from verl.utils import hf_tokenizer
 
 root_path = '/root/autodl-tmp'  # '/data1/lvnuoyan' 
 model_path = 'tictactoe-math'
+# model_path = 'tictactoe-math'
 model_name = 'game40'
+# tokenizer = hf_tokenizer(f"{root_path}/{model_path}/{model_name}")
 tokenizer = hf_tokenizer(f"{root_path}/{model_path}/{model_name}")
-# tokenizer = hf_tokenizer(f"{root_path}/{model_name}")
 time_str = time.strftime("%m-%d-%H-%M", time.localtime())
 # file_name = 'game100-gsm8k-09-23-17-39.json'
 # file_name = 'Qwen2.5-1.5B-Instruct-gsm8k-09-23-17-44.json'
@@ -24,8 +24,8 @@ def load_llm():
     os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
     os.environ["CUDA_VISIBLE_DEVICES"] = '0'
     # tokenizer = AutoTokenizer.from_pretrained(config.actor_rollout_ref.model.path)
-    model = f'{root_path}/{model_path}/{model_name}'
-    # model = f"{root_path}/{model_name}"
+    # model = f'{root_path}/{model_path}/{model_name}'
+    model = f"{root_path}/{model_path}/{model_name}"
     # ro_config = config.actor_rollout_ref.rollout
     llm = LLM(
 		model,
@@ -43,8 +43,7 @@ def reformat_prompt(prompt0):
     # 将prompt句子末尾的 Let\'s think step by step and output the final answer after "####".
     # 替换为Let\'s think step by step and output your think and final answer in this format: 
     # <think> [your thought] </think> <answer> [your answer] </answer>
-    prompt = prompt0.replace("Let\'s think step by step and output the final answer after \"####\".", 
-                             "Let\'s think step by step and always output: <think> [Your thoughts] </think> <answer> [your answer] </answer> with no extra text. Strictly follow this format. Max response length: 200 words (tokens).")
+    prompt = prompt0 + "Let\'s think step by step and always output: <think> [Your thoughts] </think> <answer> [your answer] </answer> with no extra text. Strictly follow this format. Max response length: 200 words (tokens)."
     message = [{"role": "system", "content": "You're a helpful assistant. "},
                {"role": "user", "content": prompt}]
     # apply_chat_template
@@ -70,14 +69,14 @@ def test_math(llm, sampling_params, math):
     accs_strict = []
     accs_flex = []
     answers = []
-    for i in tqdm.trange(len(math['test'])):  # len(math['test'])
+    for i in tqdm.trange(len(math)):  # len(math['test'])
         # 调整prompt内容，之前的格式不太对劲，导致模型输出的最后一个数字不是最后一个数字
-        prompt = reformat_prompt(math['test'][i]['prompt'][0]['content'])# 模型推理
+        prompt = reformat_prompt(math[i]['extra_info']['question'])# 模型推理
         outputs = llm.generate(prompt, sampling_params)
         solu_strict, solu_flex = extract_solution(outputs[0].outputs[0].text)
         answers.append(outputs[0].outputs[0].text)
         # print(outputs[0].outputs[0].text)
-        ground_truth = parse(math['test'][i]['reward_model']['ground_truth'])
+        ground_truth = parse(math[i]['extra_info']['answer'])
         correct_strict = verify(parse(solu_strict), ground_truth)
         if solu_strict is None:
             accs_strict.append(None)
@@ -95,21 +94,26 @@ def test_math(llm, sampling_params, math):
             accs_flex.append(0)
     return accs_strict, accs_flex, answers
 
+
+# 复制出问题了，需要改回mathlv3-5的数据集
 if __name__ == '__main__':
     path0 = f'{root_path}/reasoning'
+    # math = datasets.load_dataset("parquet", 
+    #               data_files={'train': path0 + '/gsm8k/train.parquet', 'test': path0 + '/gsm8k/test.parquet'})
+    # # print(math['test']['prompt'][0])
     math = datasets.load_dataset("parquet", 
-                  data_files={'train': path0 + '/gsm8k/train.parquet', 'test': path0 + '/gsm8k/test.parquet'})
-    # print(math['test']['prompt'][0])
+                  data_files=path0 + '/SimpleRL-Zoo-Data/simplelr_abel_level3to5/test.parquet')['train']
     llm, sampling_params = load_llm()
     # exit(0)
     accs_strict, accs_flex, answers = test_math(llm, sampling_params, math)
     acc0 = accs_strict.count(1) / len(accs_strict)
-    print('-----------------strict mode-----------------')
+    print('-----------strict mode-----------')
     print('total acc:', acc0)
     print('invalid output:', accs_strict.count(None))
-    print('-----------------flexible mode-----------------')
+    print('-----------flexible mode---------')
     acc0 = accs_flex.count(1) / len(accs_flex)
     print('total acc:', acc0)
     # answers存储
-    with open(f'reason_test/gsm8k-{model_name}-{time_str}.json', 'w') as f:
+    with open(f'reason_test/lv3to5-{model_name}-{time_str}.json', 'w') as f:
         json.dump(answers, f)
+
